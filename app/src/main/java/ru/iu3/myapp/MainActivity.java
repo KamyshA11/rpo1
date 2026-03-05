2225694fb0655ec5578bf7359e3802e60dc7908c
Kamyshova Anastasia <39695399+KamyshA11@users.noreply.github.com>
2026-03-06 02:26:34 +0300

    part 1 is done

--- app/src/main/java/ru/iu3/myapp/MainActivity.java
+++ app/src/main/java/ru/iu3/myapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
 import ru.iu3.myapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'myapp' library on application startup.
     static {
         System.loadLibrary("myapp");
+        System.loadLibrary("mbedcrypto");
     }
 
     private ActivityMainBinding binding;
@@ -23,9 +26,21 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        int initializationStatus = initRng();
+        byte[] encryptionKey = randomBytes(16);
+        byte[] originalBytes = {5, 7, 12, 21};
+        byte[] encryptedBytes = encrypt(encryptionKey, originalBytes);
+        byte[] decryptedBytes = decrypt(encryptionKey, encryptedBytes);;
+
         // Example of a call to a native method
         TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        // tv.setText(stringFromJNI());
+
+        // tv.setText(initializationStatus);
+        tv.setText(Arrays.toString(encryptionKey));
+        // tv.setText(Arrays.toString(originalBytes));
+        // tv.setText(Arrays.toString(encryptedBytes));
+        // tv.setText(Arrays.toString(decryptedBytes));
     }
 
     /**
@@ -33,4 +48,10 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
