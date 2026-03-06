8ae88722c934b1f96ccb3ee79fb9c8065384f9fa
Kamyshova Anastasia <39695399+KamyshA11@users.noreply.github.com>
2026-03-06 11:17:12 +0300

    Part 2 is done (pinpad returns value)

--- app/src/main/java/ru/iu3/myapp/MainActivity.java
+++ app/src/main/java/ru/iu3/myapp/MainActivity.java
@@ -1,7 +1,13 @@
 package ru.iu3.myapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,6 +21,8 @@ import java.util.Arrays;
 import ru.iu3.myapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityResultLauncher activityResultLauncher;
 
     // Used to load the 'myapp' library on application startup.
     static {
@@ -42,11 +50,26 @@ public class MainActivity extends AppCompatActivity {
         // tv.setText(stringFromJNI());
 
         // tv.setText(initializationStatus);
-        tv.setText(Arrays.toString(encryptionKey));
+        // tv.setText(Arrays.toString(encryptionKey));
         // tv.setText(Arrays.toString(originalBytes));
         // tv.setText(Arrays.toString(encryptedBytes));
         // tv.setText(Arrays.toString(decryptedBytes));
         // Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+
+        activityResultLauncher  = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback() {
+                @Override
+                public void onActivityResult(Object o) {
+                    ActivityResult result = (ActivityResult) o;
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        // обработка результата
+                        String pin = data.getStringExtra("pin");
+                        Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
     }
 
     public static byte[] stringToHex(String s)
@@ -63,9 +86,20 @@ public class MainActivity extends AppCompatActivity {
         return hex;
     }
 
+    // public void onButtonClick(View v)
+    // {
+    //     byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+    //     byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+    //     byte[] dec = decrypt(key, enc);
+    //     String s = new String(Hex.encodeHex(dec)).toUpperCase();
+    //     Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    // }
+
     public void onButtonClick(View v)
     {
-        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        Intent it = new Intent(this, PinpadActivity.class);
+        // startActivity(it);
+        activityResultLauncher.launch(it);
     }
 
     /**
@@ -75,8 +109,6 @@ public class MainActivity extends AppCompatActivity {
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
-
     public static native byte[] encrypt(byte[] key, byte[] data);
-
     public static native byte[] decrypt(byte[] key, byte[] data);
 }
